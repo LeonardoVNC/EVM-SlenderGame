4c35f7e903e26d320ed1bdf2385030148614e78f
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-06 09:36:03 -0400

    movement audio and jump action

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,37 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
+    public float jumpSpeed = 5f;
     public InputActionReference sprintAction;
+    public InputActionReference jumpAction;
 
     private Vector2 movementInput;
     private Rigidbody rb;
 
+    private AudioSource audioSource;
+
+    private bool isJumping = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     void FixedUpdate()
     {
         MovePlayer();
+        isJumping = rb.linearVelocity.y >= 0.1f;
     }
 
     public void OnMovement(InputValue data) {
         movementInput = data.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue data) {
+        if(jumpAction.action.IsPressed() && !isJumping) {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpSpeed, rb.linearVelocity.z);
+        }
     }
 
     public void MovePlayer () {
@@ -31,5 +45,11 @@ public class PlayerMovement : MonoBehaviour
             movementSpeed = 5f;
         }
         rb.linearVelocity = new Vector3(direction.x * movementSpeed, rb.linearVelocity.y, direction.z * movementSpeed);
+
+        if (!isJumping && movementInput.magnitude > 0.1f && !audioSource.isPlaying) {
+            audioSource.Play();
+        } else if (isJumping || movementInput.magnitude <= 0.1f && audioSource.isPlaying) {
+            audioSource.Pause();
+        }
     }
 }
