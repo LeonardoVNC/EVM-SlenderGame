aaf2c6e3df10e4840b0324c725a681deada7f938
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-06 08:01:00 -0400

    first commit

---
+++ Assets/Scripts/PlayerLook.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensibility = 150f;
+    public Transform playerCamera;
+
+    private float xRotation = 0f;
+    private Vector2 mouseInput;
+
+    private Vector3 canPosition;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        canPosition = playerCamera.localPosition;
+    }
+
+    void Update()
+    {
+        LookAround();
+    }
+
+    public void OnLook(InputValue data){
+        mouseInput = data.Get<Vector2>();
+        LookAround();
+    }
+
+    public void LookAround(){
+        xRotation -= mouseInput.y * mouseSensibility * Time.deltaTime;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        playerCamera.localRotation = Quaternion.Euler(xRotation, 0f,0f);
+        transform.Rotate(Vector3.up * mouseInput.x * mouseSensibility * Time.deltaTime);
+    }
+}
