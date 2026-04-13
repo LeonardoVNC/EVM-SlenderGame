11e6dcb34c6adbca4d11b15a8dfd8d91d4c1acc8
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2026-04-13 08:23:47 -0400

    add slender catch behavior

--- Assets/Scripts/Slender.cs
+++ Assets/Scripts/Slender.cs
@@ -4,17 +4,21 @@ using UnityEngine.AI;
 public class Slender : MonoBehaviour
 {
     private NavMeshAgent navMeshAgentSlender;
-    private PlayerMovement player;
+    private PlayerMovement playerMovement;
+    private PlayerLook playerLook;
     private SkinnedMeshRenderer slenderMeshRenderer;
     private Animator slenderAnimator;
 
     private float baseSpeed = 0.5f;
+    private float catchDistance = 2f;
     private bool isActive = false;
+    private bool isGameOver = false;
 
     void Start()
     {
         navMeshAgentSlender = GetComponent<NavMeshAgent>();
-        player = FindAnyObjectByType<PlayerMovement>();
+        playerMovement = FindAnyObjectByType<PlayerMovement>();
+        playerLook = FindAnyObjectByType<PlayerLook>();
         slenderMeshRenderer = GetComponentInChildren<SkinnedMeshRenderer>();
         slenderAnimator = GetComponent<Animator>();
 
@@ -24,12 +28,13 @@ public class Slender : MonoBehaviour
 
     void Update()
     {
-        if (!isActive) return;
+        if (!isActive || isGameOver) return;
 
-        navMeshAgentSlender.destination = player.transform.position;
+        navMeshAgentSlender.destination = playerMovement.transform.position;
 
         float currentVelocity = navMeshAgentSlender.velocity.magnitude;
         slenderAnimator.SetFloat("speed", currentVelocity);
+        VerificarDistanciaConJugador();
     }
 
     public void cambiarDificultad(int notas) {
@@ -50,5 +55,28 @@ public class Slender : MonoBehaviour
         isActive = state;
         slenderMeshRenderer.enabled = state;
         navMeshAgentSlender.enabled = state; 
+    }
+
+    public void VerificarDistanciaConJugador() {
+        float distanceToPlayer = Vector3.Distance(transform.position, playerMovement.transform.position);
+        if (distanceToPlayer <= catchDistance) {
+            AtraparJugador();
+        }
+    }
+
+    public void AtraparJugador() {
+        isGameOver = true;
+        navMeshAgentSlender.isStopped = true;
+        navMeshAgentSlender.velocity = Vector3.zero;
+        
+        Vector3 slenderLookAt = new Vector3(playerMovement.transform.position.x, transform.position.y, playerMovement.transform.position.z);
+        transform.LookAt(slenderLookAt);
+        slenderAnimator.SetTrigger("jumpscare");
+
+        playerMovement.enabled = false;
+        playerLook.enabled = false;
+
+        Vector3 playerLookAtSlender = transform.position + (Vector3.up*2f);
+        playerLook.transform.LookAt(playerLookAtSlender);
     }
 }
